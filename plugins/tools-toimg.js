4b911801e54b04e2ec5600917baad4e672ddd1df
Wichi <barbozasebastian408@gmail.com>
2025-12-26 19:50:29 -0400

    Actualizar tools-toimg.js

--- plugins/tools-toimg.js
+++ plugins/tools-toimg.js
@@ -1,15 +1,15 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 if (!m.quoted) {
-return conn.reply(m.chat, `❀ Debes citar un sticker para convertir a imagen.`, m)
+return conn.reply(m.chat, `🌀 Debes citar un sticker para convertir a imagen.`, m)
 }
 await m.react('🕒')
 let xx = m.quoted
 let imgBuffer = await xx.download()   
 if (!imgBuffer) {
 await m.react('✖️')
-return conn.reply(m.chat, `ꕥ No se pudo descargar el sticker.`, m)
+return conn.reply(m.chat, `💥 No se pudo descargar el sticker.`, m)
 }
-await conn.sendMessage(m.chat, { image: imgBuffer, caption: '❀ *Aquí tienes ฅ^•ﻌ•^ฅ*' }, { quoted: m })
+await conn.sendMessage(m.chat, { image: imgBuffer, caption: '💯 *Aquí tienes pendejo' }, { quoted: m })
 await m.react('✔️')
 }
 
