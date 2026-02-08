c2109092cebde384aae6bc7a3b770fe59dcbef00
Wichi <barbozasebastian408@gmail.com>
2026-02-08 00:35:51 -0400

    Actualizar donar-sala.js

--- plugins/donar-sala.js
+++ plugins/donar-sala.js
@@ -1,16 +1,29 @@
 let toM = a => '@' + a.split('@')[0]
+
 function handler(m, { groupMetadata }) {
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b
-do b = ps.getRandom()
-while (b === a)
-m.reply(`*${toM(a)},* _Busca la salita bebe que ya viene el vs_ 📌
-sᥲsᥙkᥱ ᑲ᥆𝗍 🤖`, null, {
-mentions: [a, b]
-})}
+    let ps = groupMetadata.participants.map(v => v.id)
+    let a = ps.getRandom()
+    
+    let message = `
+╭──〔 👾 *𝐊𝐄𝐈𝐒𝐓𝐎𝐏'  𝐁𝐎𝐓* 👾 〕──╮
+│
+│ 🤺 *ELECCIÓN DE HOST*
+│
+│ 👤 ${toM(a)}
+│
+│ 📝 _Bebe, busca la salita que ya_
+│ _viene el vs. ¡No te hagas el loco!_ 📌
+│
+╰───────────────────────────╯`.trim()
+
+    m.reply(message, null, {
+        mentions: [a]
+    })
+}
+
 handler.help = ['donarsala']
 handler.tags = ['freefire']
 handler.command = ['donarsala', 'sala']
 handler.group = true 
+
 export default handler
